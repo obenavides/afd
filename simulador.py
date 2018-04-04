bb0164fe6b067faff842a946fe70ceb27775846c
Oscar Benavides <oebenavidesv@gmail.com>
2018-04-04 07:52:57 -0500

    Se utiliza el diccionario mensaje.

--- simulador.py
+++ simulador.py
@@ -5,13 +5,11 @@ d['2','1']='3'
 d['3','1']='1'
 
 def AFD(d,q0,F,cinta):
-    q = q0
+    q=q0
     for simbolo in cinta:
         q=d[q,simbolo]
-    if q in F:
-        return 'Es divisible'
-    else:
-        return 'No es divisible'
+    return q in F
+mensaje={True:'Aceptada',False:'Rechazada'}
 
 for i in ('','1','11','111','1111','11111','111111'):
-    print i,AFD(d,'0',{'3','0'},i)
+    print 'La entrada '+i+' es '+mensaje[AFD(d,'0',{'3','0'},cinta)]
